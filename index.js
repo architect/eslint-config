140e8472969935bd21e6d482eee0c0c589902644
Ryan Block <rblock@gmail.com>
2021-08-21 15:25:51 -0700

    Tidy file, prob don't need 'id-length', 'max-params', 'no-var', 'quote-props', 'no-use-before-define'

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* eslint-disable filenames/match-regex */
+let off = 'off'
+let err = 'error'
 module.exports = {
   parserOptions: {
     ecmaVersion: 11 // 2020
@@ -15,50 +16,41 @@ module.exports = {
   ],
   rules: {
     // Disable rules from base configurations
-    'arrow-body-style': 'off',
-    'no-console': 'off',
-    'no-inner-declarations': 'off',
-    'no-redeclare': 'off',
-    'no-useless-escape': 'off',
+    'arrow-body-style':             off,
+    'no-console':                   off,
+    'no-inner-declarations':        off,
+    'no-redeclare':                 off,
+    'no-useless-escape':            off,
     // Enable additional rules
-    'global-require': 'error',
-    'handle-callback-err': 'error',
-    'linebreak-style': [ 'error', 'unix' ],
-    'no-cond-assign': [ 'error', 'always' ],
+    'global-require':               err,
+    'handle-callback-err':          err,
+    'linebreak-style':            [ err, 'unix' ],
+    'no-cond-assign':             [ err, 'always' ],
     // Style
-    'array-bracket-spacing': [ 'error', 'always' ],
-    'arrow-spacing': 'error',
-    'brace-style': [ 'error', 'stroustrup', { allowSingleLine: true } ],
-    'comma-spacing': [ 'error', { 'before': false, 'after': true } ],
-    'computed-property-spacing': 'error',
-    'eol-last': 'error',
-    'fp/no-class': 'error',
-    'func-call-spacing': 'error',
-    'func-style': [ 'error', 'declaration', { allowArrowFunctions: true } ],
-    'indent': [ 'error', 2 ],
-    'key-spacing': [ 'error', { beforeColon: false, mode: 'minimum' } ],
-    'keyword-spacing': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [ 'error', 'always' ],
-    'quotes': [ 'error', 'single', { allowTemplateLiterals: true, avoidEscape: true } ],
-    'require-await': 'error',
-    'semi': [ 'error', 'never' ],
-    'space-before-function-paren': 'error',
-    'space-infix-ops': 'error',
-    'spaced-comment': 'error',
-    'template-curly-spacing': 'error',
+    'array-bracket-spacing':      [ err, 'always' ],
+    'arrow-spacing':                err,
+    'brace-style':                [ err, 'stroustrup', { allowSingleLine: true } ],
+    'comma-spacing':              [ err, { before: false, after: true } ],
+    'computed-property-spacing':    err,
+    'eol-last':                     err,
+    'fp/no-class':                  err,
+    'func-call-spacing':            err,
+    'func-style':                 [ err, 'declaration', { allowArrowFunctions: true } ],
+    'indent':                     [ err, 2 ],
+    'key-spacing':                [ err, { beforeColon: false, mode: 'minimum' } ],
+    'keyword-spacing':              err,
+    'no-trailing-spaces':           err,
+    'object-curly-spacing':       [ err, 'always' ],
+    'quotes':                     [ err, 'single', { allowTemplateLiterals: true, avoidEscape: true } ],
+    'no-return-await':              err,
+    'semi':                       [ err, 'never' ],
+    'space-before-function-paren':  err,
+    'space-infix-ops':              err,
+    'spaced-comment':               err,
+    'template-curly-spacing':       err,
     // Modules must resolve
-    'import/no-unresolved': [ 'error', { commonjs: true, amd: true } ],
+    'import/no-unresolved':       [ err, { commonjs: true, amd: true } ],
     // Ensure filesystem safe filenames
-    'filenames/match-regex': [ 'error', '^[a-z0-9-_]+$', true ],
-
-    /**
-     * Maybes for later!
-     */
-    // 'id-length' // help ppl keep things descriptive
-    // 'max-params' // prob want to shoot for like 5
-    // 'no-var'
-    // 'quote-props'
-    // 'no-use-before-define'
+    'filenames/match-regex':      [ err, '^[a-z0-9-_.]+$', true ],
   }
 }
